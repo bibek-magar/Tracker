4255ac6777a1c7bb521dd5772ca68e9189412062
beevekmgr <beevekmgr@gmail.com>
2020-01-12 05:16:23 +0545

    Created an API and linked to MONGO

---
+++ tracks-server/src/index.js
@@ -0,0 +1,38 @@
+require("./models/User");
+require("./models/Tracks");
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const authRoutes = require("./routes/authRoutes");
+const trackRoutes = require("./routes/trackRoutes");
+const requireAuth = require("./middlewares/requireAuth");
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use(authRoutes);
+app.use(trackRoutes);
+
+const mongoUri =
+  "mongodb+srv://bibek123:bibek123@cluster0-1krmo.mongodb.net/test?retryWrites=true&w=majority";
+
+mongoose.connect(mongoUri, {
+  useNewUrlParser: true,
+  useCreateIndex: true
+});
+
+mongoose.connection.on("connected", () => {
+  console.log("connected to mogo instance");
+});
+
+mongoose.connection.on("error", err => {
+  console.log("Eror connecting to mongo" + err);
+});
+
+app.get("/", requireAuth, (req, res) => {
+  res.send("Hi there");
+});
+
+app.listen(3000, () => {
+  console.log("Listening on port 3000");
+});
